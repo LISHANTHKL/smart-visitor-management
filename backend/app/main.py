dc55e1a311fa4a4788a72e9f90bb272c5361f661
LISHANTHKL <lishanthkl52@gmail.com>
2026-05-25 20:22:47 +0530

    updated cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,30 +3,4 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes.visitor import router as visitor_router
 
-app = FastAPI()
-
-# CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# ROUTES
-app.include_router(visitor_router)
-
-# TEST ROUTE
-@app.get("/")
-def home():
-    return {
-        "message": "Smart Visitor Management Backend Running"
-    }
-
-# HEALTH CHECK
-@app.get("/healthz")
-def health():
-    return {
-        "status": "ok"
-    }
+# =========================================
